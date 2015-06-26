600e9fb80ff6e90466727920a12aa51868a8d15b
Kyle Vigen <kyle.vigen@clever.com>
2015-06-26 14:18:05 -0700

    Fix drone config file

---
+++ test_drone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+npm cache clean
+npm config set ca null
+npm config set registry https://clever.registry.nodejitsu.com/
+npm config set strict-ssl true
+npm config set always-auth true
+auth=`echo -n ${npmdeployusername}:${npmdeploypassword} | base64`
+npm config set _auth $auth
+sed -i.bak s/\${NODEJITSU_AUTH}/$auth/ .npmrc_docker
+npm install
+
+npm test
