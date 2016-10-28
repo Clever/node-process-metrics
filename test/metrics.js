7be79ab3c74ec611bd400653e6b29a0ea7869527
Jeffrey Nelson <jeff.nelson@clever.com>
2016-10-28 11:47:34 -0700

    Random clean up of generated code

--- test/metrics.js
+++ test/metrics.js
@@ -1,17 +1,14 @@
-const node_metrics = require("../lib/metrics");
+const nodeMetrics = require("../lib/metrics");
 const assert = require("assert");
 
 describe("pause test", () => {
-  before(() =>
-    // Start up the pause detector
-    node_metrics.log_metrics("source", 10000)
-  );
+  // Start up the pause detector
+  before(() => nodeMetrics.log_metrics("source", 10000));
 
+  it("test2", (done) => {
+    assert.equal(nodeMetrics._last_period_pause_ms, 0);
 
-  return it("test2", (done) => {
-    assert.equal(node_metrics._last_period_pause_ms, 0);
-
-    return process.nextTick(() => {
+    process.nextTick(() => {
       // Do a bunch of stuff to monopolize the event loop
       let total = 0;
       for (let i = 0; i < 100000000; i++) {
@@ -19,11 +16,10 @@ describe("pause test", () => {
       }
 
       // Confirm that the last_period_pause_ms was increased
-      return setTimeout(() => {
-        assert(node_metrics._last_period_pause_ms > 1000);
-        return done();
-      }
-        , 100);
+      setTimeout(() => {
+        assert(nodeMetrics._last_period_pause_ms > 1000);
+        done();
+      }, 100);
     });
   });
 });
