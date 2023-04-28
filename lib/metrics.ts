5057e7137338c827796cc6b9bf799f4865bfeed5
Andrew Marine <andrew.marine@clever.com>
2023-04-27 19:39:35 -0600

    replace our home grown event loop lag monitoring with the official node one

--- lib/metrics.ts
+++ lib/metrics.ts
@@ -22,40 +22,6 @@ function log_memory_usage(log) {
   log("RSS", "gauge", mem.rss);
 }
 
-// pause_detector is useful for determining if node isn't processing the event loop. There are
-// two common explanations for these pauses:
-// 1. The event loop is monopolized by one long request
-// 2. Node is garbage collecting
-// This works by sleeping for `sleep_time_ms` and then checking how long it's been since the pause
-// detector was last scheduled. If the difference between actual pause and `sleep_time_ms` is larger
-// than `pause_threshold_ms`, then we can infer that something was monopolizing the event loop.
-function start_pause_detector(log, sleep_time_ms, pause_threshold_ms) {
-  let last_time_ms = Date.now();
-
-  // This function gets called every pause_threshold_ms
-  const pause_fn = function () {
-    const current_time_ms = Date.now();
-    // pause_ms represents the "extra" time the server slept
-    const pause_ms = current_time_ms - last_time_ms - sleep_time_ms;
-
-    // If the pause is long enough, log it immediately so we can associate it with an api request
-    if (pause_ms > pause_threshold_ms) {
-      log("Pause Detected", "counter", pause_ms);
-    }
-
-    // Update the variables for the next call
-    _last_period_pause_ms += pause_ms;
-    last_time_ms = current_time_ms;
-  };
-
-  setInterval(pause_fn, sleep_time_ms);
-}
-
-function log_pauses(log) {
-  log("PauseMetric", "gauge", _last_period_pause_ms);
-  _last_period_pause_ms = 0;
-}
-
 // log_metrics logs node process metrics at the specified frequency. It also logs every time the
 // node event loop stops processing all the events for more than a second.
 module.exports.log_metrics = (
@@ -86,6 +52,27 @@ module.exports.log_event_loop_metrics = (
     });
   }, frequency_ms);
 };
+
+function log_event_loop_lag(
+  log: metricLogger,
+  frequency_ms: number,
+) {
+  let histogram: EventLoopDelayMonitor = monitorEventLoopDelay({ resolution: 100 })
+  histogram.enable()
+  setInterval(() => {
+    histogram.disable()
+
+    const data: jsonData = {}
+    histogram.percentiles.forEach((v, k) => {
+      data[k.toString()] = v
+    })
+
+    log("event-loop-lag", "gauge", histogram.mean, data)
+
+    histogram = monitorEventLoopDelay({ resolution: 100 })
+    histogram.enable()
+  }, frequency_ms)
+}
 
 module.exports.log_active_connections = (
   source: string,
